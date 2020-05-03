709e661c5bb4ab7d836f7f008e70b7b696023272
Rizaldi Kartodijoyo <rizalkartodijoyo@gmail.com>
2020-05-03 14:38:29 +0900

    Create fizzbuzz.js

---
+++ fizzbuzz.js
@@ -0,0 +1,16 @@
+var count =1;
+output =[];
+function fizzbuzz(){
+    if(count%3===0&&count%5===0){
+    output.push("fizzbuzz");
+    }
+    else if(count%3===0){
+        output.push("fizz");
+    }
+    else if(count%5===0){
+        output.push("buzz");
+    }
+    else{
+        output.push(count);
+    }}
+    
